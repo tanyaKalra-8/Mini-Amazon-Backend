1994396fd02b2732d4c4f462777562e94490cb77
Taniya <tanya.kalra200@gmail.com>
2023-03-18 21:25:23 +0530

    first commit

---
+++ src/main/java/com/ECommerce_Backend/Mini_/Amazon/MiniAmazonApplication.java
@@ -0,0 +1,13 @@
+package com.ECommerce_Backend.Mini_.Amazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAmazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAmazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ECommerce_Backend/Mini_/Amazon/MiniAmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ECommerce_Backend.Mini_.Amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAmazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
